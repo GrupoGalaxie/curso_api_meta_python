3ce021d1f0f648a6a1d3dcf4ce843ca6fec49536
Diego Cabezas <dcabezas@compulab.com.pa>
2025-04-01 15:01:21 -0500

    WEBHOOK

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request , jsonify ,render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import json
@@ -21,12 +21,7 @@ class log(db.Model):
 with app.app_context():
     db.create_all()
 
-    prueba1 = log(texto='Mensaje de prueba 1')
-    prueba2 = log(texto='Mensaje de prueba 2')
-
-    db.session.add(prueba1)
-    db.session.add(prueba2)
-    db.session.commit()
+   
 
 
 #Funcion para ordenar los registros por fecha y hora
@@ -44,7 +39,7 @@ mensajes_log = []
 
 
 #funcion para agregar mensajes y guardar en la bd
-def agregar_mensaje_log(texto):
+def agregar_mensajes_log(texto):
     mensajes_log.append(texto)
 
     #guardar el mensaje en la bd
@@ -52,6 +47,36 @@ def agregar_mensaje_log(texto):
     db.session.add(nuevo_registro)
     db.session.commit()
 
+
+#Token de verificacion para la configuracion
+TOKEN_GALAXIE = "GALAXIE"
+
+@app.route('/webhook', methods=['GET','POST'])
+def webhook():
+    if request.method == ' GET':
+        challenge = verificar_token(request)
+        return challenge
+    elif request.method == 'POST':
+        reponse = recibir_mensajes(request)
+        return reponse
+
+
+def verificar_token(req):
+    token= req.args.get('hub.verify_token')
+    challenge = req.args.get('hub.challenge')
+
+    if challenge and token == TOKEN_GALAXIE:
+        return challenge
+    else:
+        return jsonify({'error':'Token Invalido'}),401
+
+
+
+def recibir_mensajes(req):
+    req = request.get_json()
+    agregar_mensajes_log(req)
+
+    return jsonify({'message':'EVENT_RECEIVED'})
 
 
 
